672cef0badd6c26a4dae4ce5f74497f71f975bd8
Ihsannoob <88837514+Ihsannoob@users.noreply.github.com>
2026-01-03 14:12:15 +0700

    Import PaymentSection in Index.tsx
    Added PaymentSection import to Index component.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,9 +3,9 @@ import HeroSection from "@/components/HeroSection";
 import StorySection from "@/components/StorySection";
 import FeaturesSection from "@/components/FeaturesSection";
 import JoinSection from "@/components/JoinSection";
-import Footer from "@/components/Footer";
-import Donate from "@/components/Donate";
+import Footer from "@/components/Footer"
 import Membership from "@/components/MembershipSection";
+import Payment from "@/components/PaymentSection";
   
 const Index = () => {
   return (
