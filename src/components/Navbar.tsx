b9449bcfed7ecbf3f00a2fca3ea912647164b191
Ihsannoob <88837514+Ihsannoob@users.noreply.github.com>
2026-01-03 17:32:06 +0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { Menu, X } from "lucide-react";
+import { Menu, X, User, Crown, LogOut} from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
